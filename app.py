d6da2fbb5d08fb0856b32ab55a66b8a3bd56deb2
Imesh Senanayake <imeshsenanayake1996@gmail.com>
2023-02-28 01:05:00 +0530

    face id fixed

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import webbrowser
+
 import cv2
 import os
 import requests
@@ -88,45 +90,9 @@ def train_model():
     knn.fit(faces, labels)
     joblib.dump(knn, 'static/face_recognition_model.pkl')
 
-
-#### Extract info from today's attendance file in attendance folder
-# def extract_attendance():
-#     df = pd.read_csv(f'Attendance/Attendance-{datetoday}.csv')
-#     names = df['Name']
-#     rolls = df['Roll']
-#     times = df['Time']
-#     l = len(df)
-#     return names, rolls, times, l
-
-
-#### Add Attendance of a specific user
-# def add_attendance(name):
-#     username = name.split('_')[0]
-#     userid = name.split('_')[1]
-#     current_time = datetime.now().strftime("%H:%M:%S")
-#
-#     df = pd.read_csv(f'Attendance/Attendance-{datetoday}.csv')
-#     if int(userid) not in list(df['Roll']):
-#         with open(f'Attendance/Attendance-{datetoday}.csv', 'a') as f:
-#             f.write(f'\n{username},{userid},{current_time}')
-
-
-################## ROUTING FUNCTIONS #########################
-
-#### Our main page
-# @app.route('/')
-# def home():
-#     names, rolls, times, l = extract_attendance()
-#     return render_template('home.html', names=names, rolls=rolls, times=times, l=l, totalreg=totalreg(),
-#                            datetoday2=datetoday2)
-
-
 #### This function will run when we click on Take Attendance Button
 @app.route('/start', methods=['GET'])
 def start():
-    # if 'face_recognition_model.pkl' not in os.listdir('static'):
-    #     return render_template('home.html',totalreg=totalreg(),datetoday2=datetoday2,mess='There is no trained model in the static folder. Please add a new face to continue.')
-
     cap = cv2.VideoCapture(0)
     ret = True
     while ret:
@@ -136,17 +102,17 @@ def start():
             cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 20), 2)
             face = cv2.resize(frame[y:y + h, x:x + w], (50, 50))
             identified_person = identify_face(face.reshape(1, -1))[0]
-            # add_attendance(identified_person)
             cv2.putText(frame, f'{identified_person}', (30, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 20), 2,
                         cv2.LINE_AA)
         cv2.imshow('Attendance', frame)
+
         if cv2.waitKey(1) == 27:
             break
     cap.release()
-    cv2.waitKey(5000)
+    cv2.waitKey(0)
     cv2.destroyAllWindows()
-    # names, rolls, times, l = extract_attendance()
-    # return render_template('home.html',names=names,rolls=rolls,times=times,l=l,totalreg=totalreg(),datetoday2=datetoday2)
+    webbrowser.open(f'http://localhost:3000/user/{identified_person}')
+
 
 
 #### This function will run when we add a new user
@@ -164,7 +130,8 @@ def add():
     newusername = request.form['name']
     newuserid = request.form['nic']
     # userimagefolder = 'static/faces/' + newusername
-    userimagefolder = 'static/faces/' + newusername + '_' + str(newuserid)
+    userimagefolder = 'static/faces/' + str(newuserid)
+    # userimagefolder = 'static/faces/' + newusername + '_' + str(newuserid)
     if not os.path.isdir(userimagefolder):
         os.makedirs(userimagefolder)
     cap = cv2.VideoCapture(0)
